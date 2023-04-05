e5727dcda3a1e130894594989e66be26140f1003
mohafor <mkechibil@gmail.com>
2023-04-05 19:02:14 +0200

    Models, controllers, templates, routes for Type, Lovality and Role

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -8,5 +8,7 @@ app_name='catalogue'
 
 urlpatterns = [
     path('artist/', views.artist.index, name='artist_index'),
-    path('artist/<int:artist_id>/', views.artist.show, name='artist_show'),
+    path('artist/<int:artist_id>', views.artist.show, name='artist_show'),
+    path('type/', views.type.index, name='type_index'),
+    path('type/<int:type_id>', views.type.show, name='type_show'),
 ]
